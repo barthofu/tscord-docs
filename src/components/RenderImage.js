c74e3732132317633d3bcc2f9c5c4e059391eabe
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-09-19 09:52:23 +0000

    feat(docs): logger page

--- src/components/RenderImage.js
+++ src/components/RenderImage.js
@@ -13,7 +13,7 @@ export default function RenderImage({
             style={{
                 borderRadius: '5px',
                 ...(center && { display: 'block', margin: '0 auto' }),
-                boxShadow: 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.5) 0px 18px 36px -18px;',
+                boxShadow: '0 0 20px rgba(0, 0, 0, .25)',
             }}
         />
     )
